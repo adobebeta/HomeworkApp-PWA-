9f404e304f985c7e7f96bbc4c0d5d424279692b9
Nussaba Khienprasit <adobebeta1997@gmail.com>
2020-04-05 03:38:34 +0700

    add data to Firebase Cloud
    add new homework

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAs0uPzc60rGCUNWE2vssiIF4NFxPXsjqw",
+    authDomain: "doyourhomework-64c8b.firebaseapp.com",
+    databaseURL: "https://doyourhomework-64c8b.firebaseio.com",
+    projectId: "doyourhomework-64c8b",
+    storageBucket: "doyourhomework-64c8b.appspot.com",
+    messagingSenderId: "756222551723",
+    appId: "1:756222551723:web:9f22df3e3efef3ad3bb9ee",
+    measurementId: "G-R2FH73NDC3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
